61fd20c58804dd77d337539d222b0dd642fec1d4
tochiroby <tochiroby@gmail.com>
2016-11-12 22:45:27 +0900

    Add files via upload

--- callback.php
+++ callback.php
@@ -132,10 +132,10 @@ if ($text == 'はい') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
+    "altText" => "こんにちわ 何かご質問あありますか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "こんにちわ 何かご用ですか？",
+        "text" => "こんにちわ 何かご質問あありますか？",
         "actions" => [
             [
               "type" => "message",
