e8344d70a38cdf3f0cb31bca09abee23cae9c471
tochiroby <tochiroby@gmail.com>
2016-11-13 08:03:52 +0900

    Add files via upload

--- callback.php
+++ callback.php
@@ -22,6 +22,38 @@ if($type != "text"){
 if ($text == 'はい') {
   $response_format_text = [
     "type" => "template",
+    "altText" => "こちらの〇〇はいかがですか？",
+    "template" => [
+      "type" => "buttons",
+      //"thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img1.jpg",
+      "title" => "○○レストラン",
+      "text" => "お探しのレストランはこれですね",
+      "actions" => [
+          [
+            "type" => "postback",
+            "label" => "予約する",
+            "data" => "action=buy&itemid=123"
+          ],
+          [
+            "type" => "postback",
+            "label" => "電話する",
+            "data" => "action=pcall&itemid=123"
+          ],
+          [
+            "type" => "uri",
+            "label" => "詳しく見る",
+            "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
+          ],
+          [
+            "type" => "message",
+            "label" => "違うやつ",
+            "text" => "違うやつお願い"
+          ]
+      ]
+    ]
+  ];
+  /*
+    "type" => "template",
     "altText" => "最寄り駅を選択してください",
     "template" => [
       "type" => "buttons",
@@ -31,7 +63,6 @@ if ($text == 'はい') {
             "type" => "postback",
             "label" => "郡山",
             "text" => "郡山"
-
           ],
           [
             "type" => "postback",
